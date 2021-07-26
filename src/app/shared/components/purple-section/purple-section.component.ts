885cfe113998aca58c20771bfb8161f704cdc334
souad bdeir <smb46@mail.aub.edu.lb>
2021-07-26 13:09:41 +0300

    Gave the purple-section component a prop height to specify the height of each section which fixed the white space at the bottm of the page

--- src/app/shared/components/purple-section/purple-section.component.ts
+++ src/app/shared/components/purple-section/purple-section.component.ts
@@ -1,22 +1,26 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'ag-app-purple-section',
   templateUrl: './purple-section.component.html',
   styleUrls: ['./purple-section.component.scss'],
 })
-export class PurpleSectionComponent implements AfterViewInit {
-  @ViewChild('purpleContainerRef') private readonly purpleContainer!: ElementRef;
+export class PurpleSectionComponent implements AfterViewInit{
+  @Input() public height!: string;
+  @ViewChild('purpleDivRef') private readonly purpleDiv!: ElementRef;
 
   public ngAfterViewInit(): void {
+    const purpleDiv: Element = this.purpleDiv.nativeElement;
+    // setting the height
+    console.log(purpleDiv);
+    (purpleDiv as HTMLElement).style.height = this.height;
     // giving the purple section a margin equal to the height of its last child to avoid overlapping components
-    const purpleContainer: Element = this.purpleContainer.nativeElement;
-    const lastChild: Element = purpleContainer.children[this.purpleContainer.nativeElement.children.length - 1].children[0];
+    const lastChild: Element = purpleDiv.children[this.purpleDiv.nativeElement.children.length - 1].children[0];
     const childHeight: number = (lastChild as HTMLElement).offsetHeight;
 
     // give it a bottom margin unless it is the footer
     if (!lastChild.classList.contains('footer')) {
-      (this.purpleContainer.nativeElement as HTMLElement).style.marginBottom = '{childHeight}"px"';
+      (this.purpleDiv.nativeElement as HTMLElement).style.marginBottom = '{childHeight}"px"';
     }
   }
 }
