89641de88e6f7503fff1e87b7d1a0a6a1c5fdc2e
souad bdeir <smb46@mail.aub.edu.lb>
2021-08-03 21:17:04 +0300

    added action dispatch

--- src/app/shared/components/proj-list/proj-list.component.ts
+++ src/app/shared/components/proj-list/proj-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { getProjects } from 'app/shared/store/actions/project.actions';
 
 import { ProjectService } from '../../../services/project.service';
 import { Project } from '../../models/project.model';
@@ -12,9 +14,16 @@ export class ProjListComponent implements OnInit {
   public projects: Project[] = [];
   public imgBaseUrl: string = '../../../assets/images/';
 
-  public constructor(private readonly projectService: ProjectService) { }
+  public constructor(private readonly store: Store) { }
 
   public ngOnInit(): void {
-    this.projectService.getProjects().subscribe((projects: Project[]): Project[] => this.projects = projects);
+    // this.projectService.getProjects().subscribe((projects: Project[]): Project[] => this.projects = projects);
+    this.getAllProjects();
+
   }
+
+  private getAllProjects(): void{
+    this.store.dispatch(getProjects());
+  }
+
 }
